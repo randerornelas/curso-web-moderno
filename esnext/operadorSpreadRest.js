1968e8b986cdac9cf39e64fee519fef1e1a276f4
Rander Ornelas <randerornelas@gmail.com>
2021-12-31 18:15:13 -0300

    formata o documento

--- esnext/operadorSpreadRest.js
+++ esnext/operadorSpreadRest.js
@@ -2,8 +2,14 @@
 // usar rest com parâmetro de função
 
 // usar spread com objeto
-const funcionario = {nome: 'Maria', salario: 12348.99}
-const clone = {ativo: true, ...funcionario}
+const funcionario = {
+    nome: 'Maria',
+    salario: 12348.99
+}
+const clone = {
+    ativo: true,
+    ...funcionario
+}
 console.log(clone)
 
 // usar spread com array
